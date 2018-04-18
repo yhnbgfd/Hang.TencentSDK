ec18ce0bcd7d5957e8a0cf1c48da3e0497b33798
Hang <yhnbgfd@gmail.com>
2018-04-18 14:37:17 +0800

    +MessageApi.SendTemplateMessageAsync

---
+++ Hang.TencentSDK.WeiXin.MP/Apis/MessageApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hang.TencentSDK.WeiXin.MP.Apis
+{
+    class MessageApi
+    {
+        public async Task SendTemplateMessageAsync(string touser, string template_id)
+        {
+            var token = await AccessTokenManager.GetAccessTokenAsync();
+            var url = $"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={token}";
+
+            throw new NotImplementedException();
+        }
+    }
+}
